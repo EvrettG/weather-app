8018a47d46ecf4fbbc7e7283a5de5cae31f6d686
EvrettG <skyrift@tpg.com.au>
2024-04-12 19:21:49 +1000

    basic formatting and further tests of reponse from api

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,11 @@ myform.addEventListener("submit", function(event) {
     .then(function (data) {
       console.log(data);
       console.log(data.list[0].main.temp)
-    
+      console.log(data.list[0].main.humidity)
+      console.log(data.list[0].wind.speed)
+      console.log(data.list[0].weather[0].description)
     });
 });
+
+
 
